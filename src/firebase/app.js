acca4cfd55e3dbacfb5a7fac7c0fe3a0529d9ebe
George Sandeep <illustrio7077@gmail.com>
2025-12-16 22:08:44 +0530

    Removed head-ref from Git.

---
+++ src/firebase/app.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+export default function fireConf() {
+	const firebaseConfig = {
+		apiKey: "AIzaSyBByotg8m1RupfX5iB0s-zjPpkI4moKddU",
+		authDomain: "gdg-chall-25.firebaseapp.com",
+		projectId: "gdg-chall-25",
+		storageBucket: "gdg-chall-25.firebasestorage.app",
+		messagingSenderId: "388253971004",
+		appId: "1:388253971004:web:d0b625c8772ac918338939"
+	};
+
+	return firebaseConfig;
+}
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
